8f4673dc42f556f72abf2f3fb9d9f055ef3ee022
Adarsh Singh <happysingh005599@gmail.com>
2026-07-20 22:39:44 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
   // Add this block to ignore ESLint errors during deployment
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: ,
   },
   // If you also want to ignore TypeScript type-checking errors during build, add this:
   typescript: {
